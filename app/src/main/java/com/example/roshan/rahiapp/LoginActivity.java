e46ddda2d0ebfabf3e38135a44bd8de8d8ef32e5
InoTracks01 <inotracks@gmail.com>
2018-05-31 15:36:31 +0530

    GoogleSignIn

--- app/src/main/java/com/example/roshan/rahiapp/LoginActivity.java
+++ app/src/main/java/com/example/roshan/rahiapp/LoginActivity.java
@@ -10,16 +10,29 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseNetworkException;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 1;
+    private GoogleApiClient mGoogleApiClient;
 
     private EditText logEmail;
     private EditText logPass;
@@ -81,13 +94,13 @@ public class LoginActivity extends AppCompatActivity {
                                             throw task.getException();
                                         }
                                         catch(FirebaseAuthInvalidUserException e) {
-                                            Snackbar.make(view, "User Not Found, Create New Account!", Snackbar.LENGTH_SHORT).show();
+                                            Snackbar.make(view, "User Not Found, Create New Account!", Snackbar.LENGTH_LONG).show();
                                         } catch(FirebaseAuthInvalidCredentialsException e) {
-                                            Snackbar.make(view, "Invalid Password!", Snackbar.LENGTH_SHORT).show();
+                                            Snackbar.make(view, "Invalid Password!", Snackbar.LENGTH_LONG).show();
                                         } catch(FirebaseNetworkException e) {
-                                            Snackbar.make(view, "Please Check Your Internet Connection!", Snackbar.LENGTH_SHORT).show();
+                                            Snackbar.make(view, "Please Make Sure You Are Connected With Internet!", Snackbar.LENGTH_LONG).show();
                                         } catch(Exception e) {
-                                            Snackbar.make(view, "Error: "+e.getMessage(), Snackbar.LENGTH_SHORT).show();
+                                            Snackbar.make(view, "Error: "+e.getMessage(), Snackbar.LENGTH_LONG).show();
                                         }
                                     }
                                 }
@@ -95,9 +108,91 @@ public class LoginActivity extends AppCompatActivity {
                 }
                 else {
 
-                    Snackbar.make(view, "Please Enter Credentials", Snackbar.LENGTH_SHORT).show();
+                    Snackbar.make(view, "Please Enter Credentials", Snackbar.LENGTH_LONG).show();
                 }
             }
         });
+
+
+
+        //************************************GOOGLE SIGN IN************************************//
+
+        SignInButton mGoogleBtn = (SignInButton) findViewById(R.id.googleBtn);
+
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(LoginActivity.this)
+                .enableAutoManage(this, new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+                        Toast.makeText(LoginActivity.this, "Error!", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        mGoogleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mProgress.setTitle("Logging In...");
+                mProgress.setMessage("Please wait while we login...");
+                mProgress.setCanceledOnTouchOutside(false);
+                mProgress.show();
+                signIn();
+            }
+        });
+    }
+
+
+
+
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess())
+            {
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+            }else {
+                mProgress.dismiss();
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+                            mProgress.dismiss();
+                            startActivity(new Intent(LoginActivity.this,MainActivity.class)
+                                    .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+                            finish();
+                        } else {
+                            mProgress.dismiss();
+                            Toast.makeText(LoginActivity.this, "There is some problem!", Toast.LENGTH_SHORT).show();
+
+                        }
+
+                        // ...
+                    }
+                });
     }
 }
